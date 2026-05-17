0b04dcabbc44377268126a3d070a0d33585ba6ac
vinaywavy <99179212+vinaywavy@users.noreply.github.com>
2026-05-17 22:22:23 +0200

    token refresh mechanism

--- core/data/src/main/java/com/project/core/data/di/CoreDataModule.kt
+++ core/data/src/main/java/com/project/core/data/di/CoreDataModule.kt
@@ -1,10 +1,15 @@
 package com.project.core.data.di
 
+import com.project.core.data.auth.EncryptedSessionStorage
 import com.project.core.data.networking.HttpClientFactory
+import com.project.domain.session.SessionStorage
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val coreDataModule = module {
     single {
-        HttpClientFactory().build()
+        HttpClientFactory(get()).build()
     }
+    singleOf(::EncryptedSessionStorage).bind<SessionStorage>()
 }
