41d9b2245e476de6ed7b61ca2b3045dbf5fe755d
vinaywavy <99179212+vinaywavy@users.noreply.github.com>
2026-08-03 18:14:12 +0200

    unit test

--- auth/domain/src/test/kotlin/com/project/domain/UserDataValidatorTest.kt
+++ auth/domain/src/test/kotlin/com/project/domain/UserDataValidatorTest.kt
@@ -3,7 +3,10 @@ package com.project.domain
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class UserDataValidatorTest {
 //    @Test
@@ -11,9 +14,40 @@ class UserDataValidatorTest {
 //        val result = 2 + 2
 //        assertEquals(4, result)
 //    }
-
+    private lateinit var userDataValidator: UserDataValidator
     @Test
     fun anotherTest() {
         assertThat(2 + 2).isEqualTo(4)
+    }
+
+    @BeforeEach
+    fun setUp() {
+        userDataValidator = UserDataValidator(
+            patternValidator = object: PatternValidator {
+                override fun matches(value: String): Boolean {
+                    return true
+                }
+            }
+        )
+    }
+
+    @Test
+    fun validPasswordTest() {
+        val result = userDataValidator.validatePassword("Test12345")
+        assertTrue(result.hasMinLen)
+        assertTrue(result.hasDigit)
+        assertTrue(result.hasLowerCase)
+        assertTrue(result.hasUpperCase)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "Test12345, true",
+        "test7894, false",
+        "TEST7894, false",
+        "text_457, false")
+    fun validateMultiplePasswordTest(password: String, expectedValue: Boolean) {
+        val result = userDataValidator.validatePassword(password)
+        assertThat(result.isValid).isEqualTo(expectedValue)
     }
 }
